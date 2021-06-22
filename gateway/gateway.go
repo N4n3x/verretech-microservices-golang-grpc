d3db4efb0ee43a6577f07227e079b609310001aa
N4n3x <alex.hern.dev@gmail.com>
2021-06-22 11:28:55 +0200

    fix: CORS

--- gateway/gateway.go
+++ gateway/gateway.go
@@ -194,7 +194,7 @@ func loggingMiddleware(next http.Handler) http.Handler {
 func setupResponse(w *http.ResponseWriter, req *http.Request) {
 	(*w).Header().Set("Access-Control-Allow-Origin", "*")
 	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+	// (*w).Header().Set("Access-Control-Allow-Headers", "*")
 }
 
 func corsMiddleware(next http.Handler) http.Handler {
